5701d8207323b83c04cf1006d897a6d44baef066
Miguel Vargas <miguel-vargas@misena.edu.co>
2022-05-27 20:39:11 -0500

    ejercicio1

---
+++ Ejercicio_1.py
@@ -0,0 +1,6 @@
+print(" *** Bienvenidos, vamos a validar como funciona una aplicacion de control de versiones ***")
+print(" *** A continuacion podra comprobar el resultado de diversas operaciones matematicas")
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
